21e5584492ff639d32afe3e93074a7f7e68baffd
Sylvain Savajols <s.savajols@gmail.com>
2018-05-21 21:34:21 +0200

    updates

--- RRRrrr!/src/reservations/reservations.routes.js
+++ RRRrrr!/src/reservations/reservations.routes.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-
-export class ReservationsView extends React.Component {
-  render() {
-    return (<View>
-      <Text>Reservations View.</Text>
-    </View>)
+export class ReservationsRoutes {
+  getRoutes() {
+    
   }
 }
---
+++ RRRrrr!/src/rooms/rooms.service.js
@@ -0,0 +1,11 @@
+export class RoomsService {
+
+  fetchRooms() {
+    return []
+  }
+
+  fetchRoom(roomId) {
+    return null
+  }
+
+}
